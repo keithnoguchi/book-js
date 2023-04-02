15cd0dff19034d9edbb1ab8f9e7253909157dbfc
Keith Noguchi <keith@noguchi.us>
2023-04-02 08:37:50 -0700

    yarn fmt

--- ch03/src/main.js
+++ ch03/src/main.js
@@ -7,11 +7,13 @@ function findSolution(target) {
 		} else if (current > target) {
 			return null;
 		} else {
-			return find(current + 5, `(${history} + 5)`)
-				|| find(current * 3, `(${history} * 3)`);
+			return (
+				find(current + 5, `(${history} + 5)`) ||
+				find(current * 3, `(${history} * 3)`)
+			);
 		}
 	}
-	return find(1, "1");
+	return find(1, '1');
 }
 
 const answers = [
